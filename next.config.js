ff42e1fc72c8875d1e5659442ff7837c14003167
Gavyn Caldwell <gavyncaldwell@gmail>
2018-01-08 11:52:54 -0700

    added static host

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   exportPathMap: function () {
     return {
-      '/': { page: '/' }
+      '/': { page: '/' },
+      '/about': {page: '/about'},
+      '/contact-us/our-location': {page: '/contact-us/our-location'},
+      '/terms': {page: '/terms'},
+      '/privacy-policy': {page: '/privacy-policy'}
     }
   }
 }
---
+++ out/_next/a71e9a9f-44c2-4d68-9425-b5043475274d/page/terms.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/terms', function() {
+            var comp = module.exports=webpackJsonp([5],{243:function(e,t,u){e.exports=u(244)},244:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(1),r=l(n),a=u(34),d=l(a),f=u(35),o=l(f);t.default=function(){return r.default.createElement("div",null,r.default.createElement(d.default,null),"terms",r.default.createElement(o.default,null))}}},[243]);
+            return { page: comp.default }
+          })
+        
