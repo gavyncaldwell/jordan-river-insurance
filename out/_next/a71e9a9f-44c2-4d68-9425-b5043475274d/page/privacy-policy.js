9e7f5314d71fd420bedc8ffa897be0a665e49b98
Gavyn Caldwell <gavyncaldwell@gmail>
2018-01-08 11:56:41 -0700

    gitignor out

--- out/_next/a71e9a9f-44c2-4d68-9425-b5043475274d/page/privacy-policy.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/privacy-policy', function() {
-            var comp = module.exports=webpackJsonp([6],{241:function(e,t,u){e.exports=u(242)},242:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(1),a=l(n),r=u(34),d=l(r),f=u(35),c=l(f);t.default=function(){return a.default.createElement("div",null,a.default.createElement(d.default,null),"privacy",a.default.createElement(c.default,null))}}},[241]);
-            return { page: comp.default }
-          })
-        
